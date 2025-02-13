255e3b903e95992c7b31a9b1d60823d18e3f3cd8
John Paul Mahilom <mortpauljpm03@gmail.com>
2025-02-14 01:38:26 +0800

    Update Commit - v2
    COMMENTS REMOVED

--- PHP_EXERCISE_1.php
+++ PHP_EXERCISE_1.php
@@ -1,30 +1,3 @@
-<!-- 
--------------------------------------------
-INTEGRATIVE PROGRAMMING : PHP Exercise 1
-TITLE: WEATHER CONDITIONS
--------------------------------------------
-JOHN PAUL P. MAHILOM
-BSIT II-B 
-11/02/2025 
--------------------------------------------
-INSTRUCTIONS:
-
-Arrays allow you to assign multiple values to one variable. For this PHP exercise,
-write an array variable of weather conditions with the following values: 
-( rain, sunshine, clouds, hail, sleet, snow, wind. )
-
-Using the array variable for all the
-weather conditions, echo the following statement to the browser:
-
-"We've seen all kinds of weather this month. At the beginning of the month, we had
-snow and wind. Then came sunshine with a few clouds and some rain. At least we
-didn't get any hail or sleet."
-
-Don't forget to include a title for your page, both in the header and on the page itself.
-
--------------------------------------------
--->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,10 +10,6 @@ Don't forget to include a title for your page, both in the header and on the pag
     <div>
         <h1>PHP Exercise 1: Weather Conditions</h1>
         <?php
-            // From Instructions: Write an array variable of weather conditions with the following values: ( rain, sunshine, clouds, hail, sleet, snow, wind. )
-            // Define the array of weather conditions but I'll be doing it in Associative Array to Reference weather conditions by their key
-            
-            // key => value
             $weather_conditions = [
                 "rain" => "rain",
                 "sunshine" => "sunshine",
@@ -50,8 +19,6 @@ Don't forget to include a title for your page, both in the header and on the pag
                 "snow" => "snow",
                 "wind" => "wind"
             ];
-
-            // Echo the statement where the array values are dynamically inserted with styling using the associative array keys
             echo "<p>We've seen all kinds of weather this month. 
                 At the beginning of the month, we had 
                 <span class='weather-box snow'>{$weather_conditions['snow']}</span> 
